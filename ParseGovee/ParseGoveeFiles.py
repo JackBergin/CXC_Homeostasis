42dbf12a23480eb6bb1b2b9847b881efa9244b3c
Jack Bergin <81708456+JackBergin@users.noreply.github.com>
2022-05-09 10:54:53 -0400

    stress testing the current implementation of plotting

--- ParseGovee/ParseGoveeFiles.py
+++ ParseGovee/ParseGoveeFiles.py
@@ -3,20 +3,24 @@ import csv
   
 x = []
 y = []
-  
+y2 = []
+
 with open('IDFENV.csv','r') as csvfile:
     plots = csv.reader(csvfile, delimiter = ',')
       
     for row in plots:
         x.append(row[0])
-        y.append(row[2])
-  
-print(x)
-print(y)
+        y.append(row[1])
+        y2.append(row[2])
 
-#plt.bar(x, y, color = 'g')
-##plt.xlabel('Time')
-#plt.ylabel('Temperature')
-#plt.title('Temperature over Time IDF environment')
-#plt.legend()
-#plt.show()
+#Removes the first two column values that are not important
+x.pop(0)
+y.pop(0)
+y2.pop()
+
+
+plt.plot(x[0:100], y[0:100], color = 'b')
+plt.xlabel('Time', fontsize = 10)
+plt.ylabel('Temperature', fontsize = 10)
+plt.title('Temperature over Time IDF environment', fontsize = 20)
+plt.show()
