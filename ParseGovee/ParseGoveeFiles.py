a6565a67aafd7dcb18fdd78748138ef115688489
Jack Bergin <81708456+JackBergin@users.noreply.github.com>
2022-05-09 10:43:22 -0400

    The current theory did not apply to csv types

--- ParseGovee/ParseGoveeFiles.py
+++ ParseGovee/ParseGoveeFiles.py
@@ -20,26 +20,3 @@ print(y)
 #plt.title('Temperature over Time IDF environment')
 #plt.legend()
 #plt.show()
-
-
-#!/usr/bin/env python
-import numpy
-import matplotlib
-matplotlib.use('Agg')
-import matplotlib.pyplot as plt
-
-datatype=[('index',numpy.float32), ('floati',numpy.float32), 
-        ('floatq',numpy.float32)]
-filename='bigdata.bin'
-
-def main():
-    data = numpy.memmap(filename, datatype, 'r') 
-    plt.plot(data['floati'],data['floatq'],'r,')
-    plt.grid(True)
-    plt.title("Signal-Diagram")
-    plt.xlabel("Sample")
-    plt.ylabel("In-Phase")
-    plt.savefig('foo2.png')
-
-if __name__ == "__main__":
-    main()  
