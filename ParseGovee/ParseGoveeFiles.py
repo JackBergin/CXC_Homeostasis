a0707dd085c57e62083c58e2ad190e06533adc49
Jack Bergin <81708456+JackBergin@users.noreply.github.com>
2022-05-13 13:28:32 -0400

    Optimizing graphing output

--- ParseGovee/ParseGoveeFiles.py
+++ ParseGovee/ParseGoveeFiles.py
@@ -16,10 +16,27 @@ with open('IDFENV.csv','r') as csvfile:
 #Removes the first two column values that are not important
 x.pop(0)
 y.pop(0)
-y2.pop()
+y2.pop(0)
 
+#The following will be used to clean the dates for them to take up less space
+size  = len(x)
+dataClean = []
 
-plt.plot(x[0:100], y[0:100], color = 'b')
+for i in range(size):
+    dataClean.append(x[i][5:16])
+
+print(dataClean)
+
+cleanGraphDataClean = []
+cleanGraphY = []
+
+for i in range(size):
+    if(i%8==0):
+        cleanGraphDataClean.append(dataClean[i])
+        cleanGraphY.append(y[i])
+
+#plt.plot(dataClean, y, color = 'b')
+plt.plot(cleanGraphDataClean, cleanGraphY, color = 'b')
 plt.xlabel('Time', fontsize = 10)
 plt.ylabel('Temperature', fontsize = 10)
 plt.title('Temperature over Time IDF environment', fontsize = 20)
