cfa93abfdb71f1a9231e1245396ce89c23da6510
Jack Bergin <81708456+JackBergin@users.noreply.github.com>
2022-05-24 09:42:08 -0400

    Working automated graph

--- Govee_Tests/ParseGoveeFiles.py
+++ Govee_Tests/ParseGoveeFiles.py
@@ -1,43 +1,53 @@
+# Imports the necessary plotting libraries
 import matplotlib.pyplot as plt
 import csv
   
-x = []
-y = []
-y2 = []
+# Initialization of time, temperature, and humidity values
+time = []
+temp = []
+humid = []
 
+# Converts values from the csv into lists for graphing
 with open('IDFENV.csv','r') as csvfile:
     plots = csv.reader(csvfile, delimiter = ',')
-      
     for row in plots:
-        x.append(row[0])
-        y.append(row[1])
-        y2.append(row[2])
+        time.append(row[0])
+        temp.append(row[1])
+        humid.append(row[2])
 
-#Removes the first two column values that are not important
-x.pop(0)
-y.pop(0)
-y2.pop(0)
+#Removes the first column values that are the labels
+time.pop(0)
+temp.pop(0)
+humid.pop(0)
 
 #The following will be used to clean the dates for them to take up less space
-size  = len(x)
-dataClean = []
-
+size  = len(time)
+dateClean = []
 for i in range(size):
-    dataClean.append(x[i][5:16])
+    dateClean.append(time[i][5:16])
 
-print(dataClean)
-
-cleanGraphDataClean = []
-cleanGraphY = []
-
+# Creates a limited x axis range for tic marks
+xAxisTics = []
 for i in range(size):
-    if(i%8==0):
-        cleanGraphDataClean.append(dataClean[i])
-        cleanGraphY.append(y[i])
+    if(i%2048==0):
+        xAxisTics.append(dateClean[i])
 
-#plt.plot(dataClean, y, color = 'b')
-plt.plot(cleanGraphDataClean, cleanGraphY, color = 'b')
-plt.xlabel('Time', fontsize = 10)
-plt.ylabel('Temperature', fontsize = 10)
-plt.title('Temperature over Time IDF environment', fontsize = 20)
+# Casts temperature to float values from string
+tempToFloat = []  
+for j in range(size):
+    tempToFloat.append(float(temp[j]))
+
+# Stylized fonts for the graph's axies
+font = {'family': 'serif',
+        'color':  'darkred',
+        'weight': 'normal',
+        'size': 16,
+        }
+        
+
+plt.plot(dateClean, tempToFloat, 'k')
+plt.xlabel('Date and Time', fontdict = font)
+plt.ylabel('Temperature (Degrees Farenheit)', fontdict = font)
+plt.xticks(xAxisTics)
+plt.title('Temperature over Time IDF environment', fontdict = font)
 plt.show()
