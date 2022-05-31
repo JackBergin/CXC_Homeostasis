ca7d125152ed17315d31d5d4ee426cda2a97b65d
Jack Bergin <81708456+JackBergin@users.noreply.github.com>
2022-05-31 13:45:52 -0400

    Functions used to optimize code

--- Govee_Tests/ParseGoveeFiles.py
+++ Govee_Tests/ParseGoveeFiles.py
@@ -27,56 +27,6 @@ with open('Thermometer 1_export_202205311243.csv','r') as csvfile:
         temp2.append(row[1])
         humid2.append(row[2])
 
-#Removes the first column values that are the labels
-time.pop(0)
-temp.pop(0)
-humid.pop(0)
-
-time2.pop(0)
-temp2.pop(0)
-humid2.pop(0)
-
-#The following will be used to clean the dates for them to take up less space
-size  = len(time)
-dateClean = []
-for i in range(size):
-    dateClean.append(time[i][5:16])
-
-size2  = len(time2)
-dateClean2 = []
-for i in range(size2):
-    dateClean2.append(time2[i][5:16])
-
-# Creates a limited x axis range for tic marks
-xAxisTics = []
-xAxisTics.append(dateClean[size-1])
-xAxisTics.append(dateClean[0])
-xAxisTics.append(dateClean[round(size/2)])
-xAxisTics.append(dateClean[round(size/4)])
-xAxisTics.append(dateClean[round(size*3/4)])
-
-
-xAxisTics2 = []
-xAxisTics2.append(dateClean2[size2-1])
-xAxisTics2.append(dateClean2[0])
-xAxisTics2.append(dateClean2[round(size2/2)])
-xAxisTics2.append(dateClean2[round(size2/4)])
-xAxisTics2.append(dateClean2[round(size2*3/4)])
-
-
-# Casts temperature to float values from string
-tempToFloat = []  
-humidToFloat = []
-for j in range(size):
-    tempToFloat.append(float(temp[j]))
-    humidToFloat.append(float(humid[j]))
-
-tempToFloat2 = []  
-humidToFloat2 = []
-for f in range(size2):
-    tempToFloat2.append(float(temp2[f]))
-    humidToFloat2.append(float(humid2[f]))
-
 # Stylized fonts for the graph's axies
 fontAxis = {'family': 'serif',
             'color':  'darkgreen',
@@ -90,38 +40,82 @@ fontTitle = {'family': 'serif',
         'size': 12,
        }
 
-plt.figure(1)
-plt.plot(dateClean,tempToFloat, color = '#7C7978')
-plt.xlabel('Date and Time', fontdict = fontAxis)
-plt.ylabel('Temperature (\N{DEGREE SIGN}F)', fontdict = fontAxis)
-plt.xticks(xAxisTics)
-plt.title('Temperature over Time\n (IDF environment)', fontdict = fontTitle, pad = 10)
-plt.grid(True)
-plt.show()
+def cleanData(time, temp, humid):
+    #Removes the first column values that are the labels
+    time.pop(0)
+    temp.pop(0)
+    humid.pop(0)
+    
+    #The following will be used to clean the dates for them to take up less space
+    size  = len(time)
+    dateClean = []
+    for i in range(size):
+        dateClean.append(time[i][5:16])
+    
+    # Creates a limited x axis range for tic marks
+    xAxisTics = []
+    xAxisTics.append(dateClean[size-1])
+    xAxisTics.append(dateClean[0])
+    xAxisTics.append(dateClean[round(size/2)])
+    xAxisTics.append(dateClean[round(size/4)])
+    xAxisTics.append(dateClean[round(size*3/4)])
+    
+    # Casts temperature to float values from string
+    tempToFloat = []  
+    humidToFloat = []
+    for j in range(size):
+        tempToFloat.append(float(temp[j]))
+        humidToFloat.append(float(humid[j]))
 
-plt.figure(2)
-plt.plot(dateClean, humidToFloat, color = '#7C7978')
-plt.xlabel('Date and Time', fontdict = fontAxis)
-plt.ylabel('Relative Humidity (%)', fontdict = fontAxis)
-plt.xticks(xAxisTics)
-plt.title('Relative Humidity over Time\n (IDF environment)', fontdict = fontTitle, pad = 10)
-plt.grid(True)
-plt.show()
+    return tempToFloat,humidToFloat,dateClean,xAxisTics
 
-plt.figure(3)
-plt.plot(dateClean2,tempToFloat2, color = '#7C7978')
-plt.xlabel('Date and Time', fontdict = fontAxis)
-plt.ylabel('Temperature (\N{DEGREE SIGN}F)', fontdict = fontAxis)
-plt.xticks(xAxisTics2)
-plt.title('Temperature over Time\n (XR environment)', fontdict = fontTitle, pad = 10)
-plt.grid(True)
-plt.show()
+    
 
-plt.figure(4)
-plt.plot(dateClean2, humidToFloat2, color = '#7C7978')
-plt.xlabel('Date and Time', fontdict = fontAxis)
-plt.ylabel('Relative Humidity (%)', fontdict = fontAxis)
-plt.xticks(xAxisTics2)
-plt.title('Relative Humidity over Time\n (XR environment)', fontdict = fontTitle, pad = 10)
-plt.grid(True)
-plt.show()
+def plotTempIDF():
+    tempToFloat, humidToFloat, dateClean, xAxisTics = cleanData(time, temp, humid)
+    plt.figure(1)
+    plt.plot(dateClean,tempToFloat, color = '#7C7978')
+    plt.xlabel('Date and Time', fontdict = fontAxis)
+    plt.ylabel('Temperature (\N{DEGREE SIGN}F)', fontdict = fontAxis)
+    plt.xticks(xAxisTics)
+    plt.title('Temperature over Time\n (IDF environment)', fontdict = fontTitle, pad = 10)
+    plt.grid(True)
+    plt.show()
+
+def plotHumidityIDF():
+    tempToFloat, humidToFloat, dateClean, xAxisTics = cleanData(time, temp, humid)
+    plt.figure(2)
+    plt.plot(dateClean, humidToFloat, color = '#7C7978')
+    plt.xlabel('Date and Time', fontdict = fontAxis)
+    plt.ylabel('Relative Humidity (%)', fontdict = fontAxis)
+    plt.xticks(xAxisTics)
+    plt.title('Relative Humidity over Time\n (IDF environment)', fontdict = fontTitle, pad = 10)
+    plt.grid(True)
+    plt.show()
+
+def plotTempXR():
+    tempToFloat2, humidToFloat2, dateClean2, xAxisTics2 = cleanData(time2, temp2, humid2)
+    plt.figure(3)
+    plt.plot(dateClean2,tempToFloat2, color = '#7C7978')
+    plt.xlabel('Date and Time', fontdict = fontAxis)
+    plt.ylabel('Temperature (\N{DEGREE SIGN}F)', fontdict = fontAxis)
+    plt.xticks(xAxisTics2)
+    plt.title('Temperature over Time\n (XR environment)', fontdict = fontTitle, pad = 10)
+    plt.grid(True)
+    plt.show()
+
+def plotHumidityXR():
+    tempToFloat2, humidToFloat2, dateClean2, xAxisTics2 = cleanData(time2, temp2, humid2)
+    plt.figure(4)
+    plt.plot(dateClean2, humidToFloat2, color = '#7C7978')
+    plt.xlabel('Date and Time', fontdict = fontAxis)
+    plt.ylabel('Relative Humidity (%)', fontdict = fontAxis)
+    plt.xticks(xAxisTics2)
+    plt.title('Relative Humidity over Time\n (XR environment)', fontdict = fontTitle, pad = 10)
+    plt.grid(True)
+    plt.show()
+
+plotTempIDF()
+plotHumidityIDF()
+plotTempXR()
+plotHumidityXR()
